0201a758632ae914a0197e76019e4809331b1b7c
Lincoln Nowell <lincolnnowell@gmail.com>
2019-03-06 16:04:38 -0600

    Add files via upload

--- build-Tic_tac_toe-Desktop_Qt_5_12_0_MSVC2017_64bit-Release/release/moc_mainwindow.cpp
+++ build-Tic_tac_toe-Desktop_Qt_5_12_0_MSVC2017_64bit-Release/release/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../Tic_tac_toe/mainwindow.h"
+#include "../../Tic_Tac_Toe/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
